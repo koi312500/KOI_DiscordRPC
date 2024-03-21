c5eecac5765b8c4f51417397b42ff625d10b06f8
KOI3125 <koi312500@gmail.com>
2024-03-21 09:34:49 +0900

    feat: change expression

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import rpc
-from pypresence import Presence
 import time
 from time import mktime
 
@@ -9,27 +8,30 @@ client_id = '865396378837975072'
 start_time = mktime(time.localtime())
 rpc_obj = rpc.DiscordIpcClient.for_platform(client_id) 
 
+assets = {
+    "small_text": "현재 공부중입니다...",  # anything you like
+    "small_image": "koi3125_profile2",  # must match the image key
+    "large_text": "공부중.....",  # anything you like
+    "large_image": "studylogo"  # must match the image key
+}
+
 print("Program will ask forever. If you want to keep your rpc state, please don't input anything.")
+
 while 1:
     detatils_now = str(input("Please input you RPC's details : "))
     state_now = str(input("Please input you RPC's state : "))
-    timestamps_change = str(input("Do you want to change you timestamps to now? (Input 'y' to change. Other char == None.) : "))
-    if str(timestamps_change) == 'y':
+    timestamps_change = str(input("Do you want to change your timestamps to now? Input 'y' to change. : "))
+
+    if timestamps_change == 'y':
         start_time = mktime(time.localtime())
+
     activity = {
             "state": state_now,  # anything you like
             "details": detatils_now,  # anything you like
             "timestamps": {
                 "start": start_time
             },
-            "assets": {
-                "small_text": "코이를 낚는 낚시꾼 코이!",  # anything you like
-                "small_image": "koi3125_profile-512",  # must match the image key
-                "large_text": "코이는 코이에요!",  # anything you like
-                "large_image": "koi3125_profile2"  # must match the image key
-            },
-            "buttons": [{"label": "Join to KOI's server.", "url": "https://discord.gg/sX2K7eGdzT"}, {"label": "KOI's solved.ac!", "url": "https://solved.ac/profile/koi312500"}]
+            "assets": assets,
+            "buttons": [{"label": "코이의 서버에요!", "url": "https://discord.gg/mcBjTMMxN6"}, {"label": "코이의 사이트", "url": "https://koi3125.xyz"}]
         }
     rpc_obj.set_activity(activity)
-    
-from pypresence import Presence
